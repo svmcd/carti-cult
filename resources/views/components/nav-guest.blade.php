df205564456f8b0af9400a2bdbc0a9cdfc53999d
svmcd <spolat0750@gmail.com>
2022-05-23 14:52:43 +0200

    guest- nav, page and footer done.

---
+++ resources/views/components/nav-guest.blade.php
@@ -0,0 +1,5 @@
+<div class="nav-guest">
+    <div>
+        <a href="#"><div>LOGIN</div></a>
+    </div>
+</div>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,9 +7,6 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
@@ -17,8 +14,10 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
+        @include('components.nav-guest')
+        <div class="guest-content">
             {{ $slot }}
         </div>
+        @include('components.footer-guest')
     </body>
 </html>
